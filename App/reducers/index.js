d6ae5ea2cdb54fa2999abd6e7c39fd24082c235e
vinitraj10 <ent.vinit01@gmail.com>
2017-08-21 00:55:31 +0530

    Fixing repo

--- App/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-//Our reducers for every News tags.
-import news from './newsReducer';
-
-const rootReducer = combineReducers({
-	news:news
-});
-
-export default rootReducer;
