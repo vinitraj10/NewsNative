11c13823e73e42387693de5d6a747d25eabc88e1
vinitraj10 <ent.vinit01@gmail.com>
2017-08-21 00:31:57 +0530

    News Tags container file

---
+++ App/containers/tags.js
@@ -0,0 +1,37 @@
+import React,{Component} from 'react';
+import {View,StyleSheet,Text} from 'react-native';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as actions from '../../actions';
+import Section from '../../components/Section';
+import Button from '../../components/Button';
+
+class Details extends Component{
+	render(){
+		console.log(this.props);
+		const {tagsContainer,btn} = styles;
+		return (
+			<Section>
+				<View style={tagsContainer}>
+					<Button title='Buisness' color='#5cb728' method={this.props.BnsNews}/>
+					<Button title='Entertainment' color='#2887b7' method={this.props.EntNews}/>
+					<Button title='Hack' color='#e04545' method={this.props.HckrNews}/>
+				</View>
+				<View style={tagsContainer}>
+					<Button title='Indian' color='#007aff' method={this.props.IndNews}/>
+					<Button title='Sports' color='#cc336b' method={this.props.SptsNews}/>
+					<Button title='World' color='#bacc33' method={this.props.WldNews}/>
+				</View>
+			</Section>
+		);
+	}
+}
+const styles =StyleSheet.create({
+	tagsContainer:{
+		justifyContent:'space-around',
+		flexDirection:'row',
+		margin:20
+	},
+})
+
+export default connect(null,actions)(Details);
