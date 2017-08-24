62004565487ca2bc007dfac239a18ea4bdde5d74
vinitraj10 <ent.vinit01@gmail.com>
2017-08-24 23:44:40 +0530

    Fixed Intial state

--- app/reducers/intialState.js
+++ app/reducers/intialState.js
@@ -5,12 +5,3 @@ const intialState = {
 };
 export default intialState;
 
-/*
-bns_active:false,
-	ent_active:false,
-	hckr_active:false,
-	ind_active:false,
-	spts_active:false,
-	wld_active:false,
-
-*/
