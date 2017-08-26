5012c4363647d629192f80a64aa30bbd2e0d36e8
vinitraj10 <ent.vinit01@gmail.com>
2017-08-26 21:47:15 +0530

    Image Loading in news!!

--- app/containers/data.js
+++ app/containers/data.js
@@ -1,9 +1,19 @@
 import React,{Component} from 'react';
-import {ScrollView,Text,StyleSheet} from 'react-native';
+import {ScrollView,Text,View,StyleSheet,Image} from 'react-native';
 
 class Data extends Component{
 	renderNews(article){
-		return <Text key={article.title}>{article.title}</Text>
+		return(
+			<View style={styles.container} key={article.title}>
+				<View style={styles.childContainer}>
+					<Image 
+					style={styles.imgStyle}
+					source={{uri:article.urlToImage}}
+					/>
+				</View>
+				<Text>{article.title}</Text>
+			</View>
+		);	
 	}
 	render(){
 		const {articles} = this.props.data;
@@ -14,5 +24,26 @@ class Data extends Component{
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container:{
+		flexDirection:'row',
+		margin:10,
+		padding:10,
+		borderColor:"#ddd",
+		borderWidth:1,
+		borderRadius: 2,
+	},
+	childContainer:{
+		margin:5,
+		borderColor:"#ddd",
+		borderWidth:1,
+		borderRadius:2,
+	},
+	imgStyle:{
+		width:50,
+		height:50
+	}
+});
 
 export default Data;
